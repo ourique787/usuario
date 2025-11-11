e9487d56ca544875f9c1ad0c6628a89782f12026
ourique787 <lucastrajano93@gmail.com>
2025-11-11 13:43:40 -0300

    add get, delete e login

--- src/main/java/com/ourique/usuario/business/UsuarioService.java
+++ src/main/java/com/ourique/usuario/business/UsuarioService.java
@@ -4,8 +4,11 @@ package com.ourique.usuario.business;
 import com.ourique.usuario.business.converter.UsuarioConverter;
 import com.ourique.usuario.business.dto.UsuarioDTO;
 import com.ourique.usuario.infrastructure.entity.Usuario;
+import com.ourique.usuario.infrastructure.exceptions.ConflictException;
+import com.ourique.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.ourique.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,9 +17,31 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+        emailExiste(usuarioDTO.getEmail());
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
         return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+    }
+
+    public void emailExiste(String email) {
+        if (verificaEmailExistente(email)) {
+            throw new ConflictException("Email já cadastrado: " + email);
+        }
+    }
+
+    public boolean verificaEmailExistente(String email){
+        return usuarioRepository.existsByEmail(email);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email){
+        return usuarioRepository.findByEmail(email).orElseThrow(()
+                -> new ResourceNotFoundException("Email não encontrado" + email));
+    }
+
+    public void deletaUsuarioPorEmail(String email){
+        usuarioRepository.deleteByEmail(email);
     }
 }
