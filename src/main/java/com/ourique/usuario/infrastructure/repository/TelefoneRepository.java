864eca464e67ac111e5baf5118c14a98487c51af
ourique787 <lucastrajano93@gmail.com>
2025-11-11 07:30:00 -0300

    add classes security

---
+++ src/main/java/com/ourique/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.ourique.usuario.infrastructure.repository;
+
+import com.javacourse.aprendendo_spring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
